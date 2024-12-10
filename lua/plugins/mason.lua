e75d2785cae20190a55d06f21c75138a26a0ef19
Andrew Beno <benodrew12@gmail.com>
2024-12-10 14:06:50 -0500

    yaml lsp

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -20,6 +20,7 @@ return {
           "html",
           "ts_ls",
           "vuels",
+          "yamlls",
 	  -- "vue-language-server",
 	  -- ^ not allowed in ensure_installed. just run ":MasonInstall vue-language-server
           "tailwindcss",
