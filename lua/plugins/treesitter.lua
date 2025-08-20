9922188a66f1cff9fc80dc8d844b98edee138da5
Andrew Beno <benodrew12@gmail.com>
2025-08-20 17:22:50 -0400

    allow vim regex highlighting for DOcker

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -12,7 +12,7 @@ return {
     auto_install = true,
     highlight = {
       enable = true,
-      additional_vim_regex_highlighting = false,
+      additional_vim_regex_highlighting = { "dockerfile" },
     },
     incremental_selection = { enable = true },
     indent = { enable = true },
