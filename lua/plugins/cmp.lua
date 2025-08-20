57f75d7669599ae133455f4f7d91abee6a7b61b5
Andrew Beno <benodrew12@gmail.com>
2025-08-20 13:56:22 -0400

    pin cmp back to working version

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,7 +1,10 @@
 return {
   "hrsh7th/nvim-cmp",
   dependencies = {
-    "hrsh7th/cmp-nvim-lsp",
+    {
+      "hrsh7th/cmp-nvim-lsp",
+      commit="99290b3",
+    },
     "hrsh7th/cmp-buffer",
     "hrsh7th/cmp-path",
     "L3MON4D3/LuaSnip",
